6f3b461503973e2bc77c56689c54b5c9f975d99a
Daviczrn07 <davi.cezarino@edu.senai.br>
2026-09-11 09:57:50 -0300

    exer3

---
+++ AnimalZoo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca
+{
+    public class AnimalZoo
+    {
+        public virtual void EmitirSom()
+        {
+            Console.WriteLine("animal emitindo som");
+        }
+    }
+    public class Leao : AnimalZoo
+    {
+        public override void EmitirSom()
+        {
+            Console.WriteLine("Leão: Roar!");
+        }
+    }
+
+    public class Macaco : AnimalZoo
+    {
+        public override void EmitirSom()
+        {
+            Console.WriteLine("Macaco: UGH UGH AAH AAH!");
+        }
+    }
+
+    public class Cobra : AnimalZoo
+    {
+        public override void EmitirSom()
+        {
+            Console.WriteLine("Cobra: SSSSSSSSSSSSS!");
+        }
+    }
+}
