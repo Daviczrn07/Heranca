eb04a1a52c90d2f5bda75c5cb712825e9be23aa9
Daviczrn07 <davi.cezarino@edu.senai.br>
2026-09-11 08:34:15 -0300

    Adicionar arquivos de projeto.

---
+++ Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herança
+
+{
+    public class Animal
+    {
+        public string Nome { get; set; }
+
+        public void Comer()
+        {
+            Console.WriteLine("O Animal está comendo");
+        }
+    }
+
+    public class Gato : Animal
+    {
+        public void Miar()
+        {
+            Console.WriteLine("Miau, Meow!");
+        }
+    }
+
+    public class Cachorro : Animal
+    {
+        public void Latir()
+        {
+            Console.WriteLine("Au Au!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Herança
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro meuDog = new Cachorro();
+
+            meuDog.Nome = "Brunão";
+            Console.WriteLine($"Cachorro: {meuDog.Nome}");
+            meuDog.Comer();
+            Console.WriteLine($"{meuDog.Nome}:");
+            meuDog.Latir();
+
+            Gato meuGato = new Gato();
+
+            meuGato.Nome = "Davizão";
+            Console.WriteLine($"Gato: {meuGato.Nome}");
+            meuGato.Comer();
+            Console.WriteLine($"{meuGato.Nome}:");
+            meuGato.Miar();
+
+
+        }
+    }
+}
