07f1d7727ed9dfab4cb0db3278fe9911ff7f8677
Daviczrn07 <davi.cezarino@edu.senai.br>
2026-09-11 11:07:39 -0300

    Exer3

---
+++ FormaGeometrica.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca
+{
+    abstract class FormaGeometrica
+    {
+        public abstract double CalcularArea();
+    }
+}
